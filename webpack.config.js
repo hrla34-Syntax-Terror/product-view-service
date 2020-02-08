1b1fec65c5078a55be3dbb96325a54f169cbbea4
Kaleb Lee <46289359+kayrub@users.noreply.github.com>
2020-02-08 15:04:08 -0800

    Deleted node modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 const SRC_DIR = path.join(__dirname, '/client/src');
 const DIST_DIR = path.join(__dirname, '/client/dist');
@@ -10,15 +11,28 @@ module.exports = {
     path: DIST_DIR,
   },
   module: {
-    loaders: [
+    rules: [
       {
-        test: /\.jsx?/,
-        include: SRC_DIR,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015'],
-        },
+        test: /\.js[x]?/s,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+            plugins: ["@babel/plugin-proposal-class-properties"]
+          }
+        }
       },
+      {
+        test: /\.(scss|css)$/,
+        loaders: ['style-loader', 'css-loader'],
+      }
     ],
   },
+  node: {
+    fs: 'empty'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
 };
